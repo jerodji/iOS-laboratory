ffd1b49c7326ad3c99227ad9d8d547911367f9dc
jijiudong <jjd510@163.com>
2021-10-12 17:30:40 +0800

    属性string为什么用copy

---
+++ CodeDemo/copy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  copy
+//
+//  Created by DZ0400645 on 2021/10/12.
+//  Copyright © 2021 Jerod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
